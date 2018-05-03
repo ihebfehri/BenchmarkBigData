f890047983420eccb810a8267b4a2ca69a696093
Iheb Fehri <ihebfehri17@gmail.com>
2018-05-03 16:56:14 +0200

    added random time, but briken. to fix

--- src/main/scala/Spark/RealGenerator.scala
+++ src/main/scala/Spark/RealGenerator.scala
@@ -1,11 +1,13 @@
 package Spark
 
+import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.util.Random
+import java.util.{Calendar, Random}
 
 import org.apache.spark.{SparkConf, SparkContext, sql}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import java.time.temporal.ChronoUnit.DAYS
+
 
 /**
   * spark-submit  --master yarn --class Spark.localyGeneratedData localGen.jar "2017" "2018" "100" "ft_validations"
@@ -38,10 +40,10 @@ object RealGenerator extends App{
     .getOrCreate()
 
   override def main(args: Array[String]): Unit = {
-    generateRandomData(100)
+    generateRandomData(1000000)
   }
   // todo: GET THE WAREHOUSE TO WORK GOD DAMN ITS NOT CONNECTING TO REMOTE SERVER
-  // THE CODE WORKS, I DONT KNOW WHICH DB ITS WORKING ON BUT IT WORKS
+  // waiting for new server........
 
 
   //    sparkSession.sql(
@@ -68,7 +70,21 @@ object RealGenerator extends App{
     from.plusDays(random.nextInt(diff.toInt))
   }
 
-  var rowList: (Int, Int, String, Any, Int, LocalDate, Int) = _
+  def randomTimeFromRange(): String = {
+    val today = Calendar.getInstance.getTime
+    val r = scala.util.Random
+
+    // create the date/time formatters
+    val minuteFormat = new SimpleDateFormat("mm")
+    val hourFormat = new SimpleDateFormat("hh")
+
+    val currentHour = hourFormat.format(r.nextInt(23))      // 12
+    val currentMinute = minuteFormat.format(r.nextInt(59))  // 29
+
+    "%s:%s".format(currentHour, currentMinute)
+  }
+
+  var rowList: (Int, Int, String, Any, Int, LocalDate, String, Int) = _
   def generateRandomData(numberofLines : Int): Unit = {
     val listTransport = Array("Bus", "Metro", "RER", "Tram", "Bateau", "Bus_Nuit")
     val r = scala.util.Random
@@ -82,7 +98,7 @@ object RealGenerator extends App{
         listTransport(r.nextInt(5)), //string
         r.nextInt(31),   // int
         randomDateFromRange(LocalDate.of(2017,1,1), LocalDate.of(2017,12,30)) , //localdate
-        //        ,
+        randomTimeFromRange(),
         r.nextInt(5)+1 //int
       )
       println(rowList)
